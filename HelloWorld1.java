c64865118799ed5eaafd33e71e924310e8d47b4d
mohammad <you@example.com>
2017-10-03 19:19:51 -0400

    booleans

---
+++ HelloWorld1.java
@@ -0,0 +1,18 @@
+public class HelloWorld1 
+{
+	static String randomString = "baz";
+	static final double funNum = 1.0;
+
+	public static void main(String[] args)
+	{
+		boolean isOver21 = false;
+
+		if(isOver21 == true){
+			System.out.println("you can't drink");
+		}else{
+			System.out.println("heres a beer");
+		}
+
+		
+	}
+}
