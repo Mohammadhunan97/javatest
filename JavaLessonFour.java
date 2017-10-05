327a7ec3599fe2968e14c3628140d1799bf629ca
mohammad <you@example.com>
2017-10-05 00:18:00 -0400

    while loop same as javascript

---
+++ JavaLessonFour.java
@@ -0,0 +1,14 @@
+import java.util.*; 
+
+public class JavaLessonFour
+{
+	public static void main(String args[])
+	{
+		int i = 1;
+		while(i < 20){
+			System.out.println(i + "\n");
+			i++;
+		}
+		
+	}
+}
