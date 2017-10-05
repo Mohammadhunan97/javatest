cc0473b8cde45424f928116737919db8bd221192
mohammad <you@example.com>
2017-10-05 01:25:28 -0400

    more scanner

--- JavaLessonFour.java
+++ JavaLessonFour.java
@@ -2,21 +2,18 @@ import java.util.*;
 
 public class JavaLessonFour
 {
+	static Scanner userinput = new Scanner(System.in);
+
 	public static void main(String args[])
 	{
-		// int i = 1;
-		// while(i < 20){
-		// 	System.out.println(i + "\n");
-		// 	i++;
-		// }
+		String YorN = "Y";
 
-		// for(int i = 0; i <= 20; i++){
-		// 	System.out.print(i + "\t");
-		// }
-		// System.out.println("");
-		for(int i = 0; i <= 100; i++){
-			String return_value = (i%15 == 0)? "fizzbuzz": (i%3 == 0)? "fizz" : (i%5 == 0)? "buzz": Integer.toString(i);
-			System.out.println(return_value);
+		int h = 1;
+
+		while(YorN.equalsIgnoreCase("y")){
+			System.out.print("Continue y or n?: ");
+			YorN = userinput.nextLine();
 		}
 	}
 }
+
